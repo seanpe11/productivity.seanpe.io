674de1775a9b611aa46903a607c4796e136c2a7f
Sean Pe <sean.m.s.pe@gmail.com>
2024-09-16 16:48:24 -0500

    fix key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,9 +49,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <div className="flex flex-col gap-4 sm:grid-cols-2 md:gap-8">
-          {countdowns?.map((countdown) => (
+          {countdowns?.map((countdown, index) => (
             <div
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              key={index + countdown.name}
             >
               <h3 className="text-2xl font-bold">{countdown.name}</h3>
               <div className="text-lg">
