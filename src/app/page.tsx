adbcdcabfd77faf6270a3a0750fae3da05616aeb
Sean Pe <sean.m.s.pe@gmail.com>
2024-09-24 17:04:53 -0500

    finalize crud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,26 @@ import { api } from "~/trpc/react";
 import { PuffLoader } from "react-spinners";
 import CountdownList from "~/components/CountdownList";
 import DateTimePicker from "~/components/DateTimePicker";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Home() {
+  const queryClient = useQueryClient();
+  const [isMutationLoading, setIsMutationLoading] = useState(false);
   const { data: deadlines } = api.deadline.getAll.useQuery();
-  const { mutate: createDeadline } = api.deadline.create.useMutation();
-  const { mutate: deleteDeadline } = api.deadline.delete.useMutation();
-  console.log(deadlines);
+  const { mutate: createDeadline } = api.deadline.create.useMutation({
+    onMutate: () => { setIsMutationLoading(true); setShowAddModal(false); },
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+      setIsMutationLoading(false);
+    }
+  });
+  const { mutate: deleteDeadline } = api.deadline.delete.useMutation({
+    onMutate: () => { setIsMutationLoading(true); setShowDeleteModal(false); },
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+      setIsMutationLoading(false);
+    }
+  });
 
   const handleCreate = (name: string, deadline: Date) => {
     createDeadline({ name, deadline });
@@ -53,7 +67,7 @@ export default function Home() {
     )
   }
 
-  const deleteModal = () => {
+  const DeleteModal = () => {
     return (
       <div className="fixed top-0 left-0 w-screen h-screen bg-black/50 flex items-center justify-center z-50">
         <div className="bg-white rounded-xl p-4 flex flex-col gap-4 text-black">
@@ -62,6 +76,16 @@ export default function Home() {
             <button className="bg-white px-4 py-2 rounded-xl" onClick={() => { setShowDeleteModal(false) }}>Cancel</button>
             <button className="bg-white px-4 py-2 rounded-xl" onClick={() => { deleteDeadline({ name: deadlineName, deadline: deadlineDate }) }}>Delete</button>
           </div>
+        </div>
+      </div>
+    )
+  }
+
+  const MutationLoader = () => {
+    return (
+      <div className="fixed top-0 left-0 w-screen h-screen bg-black/50 flex items-center justify-center z-50">
+        <div className="rounded-xl p-4 flex flex-col gap-4 text-black">
+          <PuffLoader color="white" loading={true} />
         </div>
       </div>
     )
@@ -78,8 +102,11 @@ export default function Home() {
         <CountdownList deadlines={deadlines}
           onDelete={handleDelete}
         />
+        {}
       </div>
       {showAddModal && <AddModal />}
+      {showDeleteModal && <DeleteModal />}
+      {isMutationLoading && <MutationLoader />}
     </main>
   );
 }
